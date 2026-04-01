be35ab2d8a759f7acbcd85f9e49dc39ee60c5584
Kyleigh Giles-Brown <kyleighgilesbrown@gmail.com>
2026-04-01 13:55:21 -0400

    connections

--- ContactApp1/ViewController1.swift
+++ ContactApp1/ViewController1.swift
@@ -9,11 +9,7 @@ import UIKit
 
 class ViewController1: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    
     
 
     /*
@@ -30,6 +26,54 @@ class ViewController1: UIViewController {
     @IBOutlet weak var sgmtEditMode: UISegmentedControl!
     
     @IBAction func changeEditMode(_ sender: Any) {
+        let textFields: [UITextField] = [txtName, txtAddress, txtCity, txtState, txtZip, txtPhone, txtCell, txtEmail]
+        if sgmtEditMode.selectedSegmentIndex == 0 {
+            for textField in textFields {
+                textField.isEnabled = false
+                textField.borderStyle = UITextField.BorderStyle.none
+            }
+            btnChange.isHidden = true
+        }
+        else if sgmtEditMode.selectedSegmentIndex == 1 {
+            for textField in textFields {
+                textField.isEnabled = true
+                textField.borderStyle = UITextField.BorderStyle.roundedRect
+            }
+            btnChange.isHidden = false
+        }
     }
+    
+    @IBOutlet weak var txtName: UITextField!
+    
+    @IBOutlet weak var txtAddress: UITextField!
+    
+    @IBOutlet weak var txtCity: UITextField!
+    
+    @IBOutlet weak var txtState: UITextField!
+    
+    @IBOutlet weak var txtZip: UITextField!
+    
+    @IBOutlet weak var txtCell: UITextField!
+    
+    @IBOutlet weak var txtPhone: UITextField!
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    @IBOutlet weak var lblBirthdate: UILabel!
+    
+    @IBOutlet weak var btnChange: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.changeEditMode(self)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
     
 }
